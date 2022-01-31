34f4a5bd7f8a904273da7214ae94fc3c6e65045f
jefferson250598 <amarantegeraldelli@gmail.com>
2022-01-31 15:23:49 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// Crie um objeto que receba ao menos três propriedades sobre você.
+
+let pessoa ={
+    nome: "Jefferson"
+    idade: 23
+    Nacionalidade:"Barsileiro"
+}
+console.log(pessoa)
+
+
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+
+pessoa.genero = "Masculino"
+console.log(pessoa)
+
+// Remova uma propriedade desse objeto.
+
+delete pessoa.genero
+console.log(pessoa)
+
+//Mostre no console todas as propriedades desse objeto.
+
+console.log(pessoa)
+
+
+
+
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+
+let cadastro ={
+    nome:"jefferson"
+    idade: 23
+    telefone: 21968645662
+    amigos:["tati","gabriel","milena","jessica"]
+}
+console.log(cadastro)
+
+
+
+
+// Mostre no console o nome de um amigo de cada lista
+console.log(amigo[0])
